9f2122825b167e0c7cf5d452eedef1a7be8aabff
Viorel Stirbu <viorels@gmail.com>
2019-03-23 02:08:29 +0200

    async sonar and rotation

--- robot_2wd.ino
+++ robot_2wd.ino
@@ -3,22 +3,45 @@
 #include "robot_pins.h"
 
 #define MAX_SONAR_DISTANCE 200  // can't do more than 450-500
-
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_SONAR_DISTANCE);
-
-void setup() {
-  Serial.begin(9600);
-
-  pinMode(LEFT_FWD_PIN, OUTPUT);
-  pinMode(LEFT_BACK_PIN, OUTPUT);
-  pinMode(RIGHT_FWD_PIN, OUTPUT);
-  pinMode(RIGHT_BACK_PIN, OUTPUT);
-}
 
 #define LEFT 0
 #define RIGHT 1
 #define FWD 0
 #define BACK 1
+
+unsigned long timer_1s = 0;
+unsigned long timer_rotate = 0;
+
+void set_timer(unsigned long &timer, int duration) {
+  timer = millis() + duration;
+}
+
+bool check_timer(unsigned long &timer) {
+  if (timer != 0 && millis() > timer) {
+    timer = 0;
+    return true;
+  }
+  return false;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(LEFT_FWD_PIN, OUTPUT);
+  pinMode(LEFT_BACK_PIN, OUTPUT);
+  pinMode(RIGHT_FWD_PIN, OUTPUT);
+  pinMode(RIGHT_BACK_PIN, OUTPUT);
+  pinMode(LEFT_PWM_PIN, OUTPUT);
+  pinMode(RIGHT_PWM_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  int setup_duration = 30;  // wait 30ms for sonar
+  set_timer(timer_1s, setup_duration);
+
+  sonar.ping_timer(echoCheck);  // async
+  delay(setup_duration);
+}
 
 int wheel_pins[][2] = {
   {LEFT_FWD_PIN, RIGHT_FWD_PIN},
@@ -79,13 +102,12 @@ void rotate_bot_angle(float angle) {  // positive for clockwise, negative for co
     angle = abs(angle);
   }
 
+  int duration = angle / 360 * milis_per_rotation;
+  set_timer(timer_rotate, duration);
+
   rotate_bot(dir, 1);
-  delay(angle / 360 * milis_per_rotation);
-  stop_bot();
 }
 
-bool moving = false;
-bool obstacle = false;
 unsigned long last_buzz = 0;
 
 #define NOTE_C3  131
@@ -100,27 +122,66 @@ void sonar_buzz(int distance) {
   }
 }
 
-void loop() {
-  int distance = sonar.ping_cm();
-  if (distance > 0) {  // if distance is 0 then nothing is in range OR it's touching an obstacle
+int distance = 0;
+
+void echoCheck() {  // Timer2 interrupt calls this function every 24uS where you can check the ping status.
+  // Don't do anything here!
+  if (sonar.check_timer()) {  // This is how you check to see if the ping was received.
+    distance = sonar.ping_result / US_ROUNDTRIP_CM;  // Ping returned, uS result in ping_result, convert to cm with US_ROUNDTRIP_CM.
     Serial.println(distance);
+  }
+  // Don't do anything here!
+}
+
+#define STOPPED 0
+#define MOVING 1
+#define ROTATING 2
+
+int state = STOPPED;
+bool obstacle = false;
+
+void loop() {
+  sonar.ping_timer(echoCheck);  // async
+
+  if (distance > 0) {  // if distance is 0 then nothing is in range OR it's touching an obstacle
     sonar_buzz(distance);
   }
   obstacle = distance != 0 && distance < 10;
 
-  if (!moving) {
-    if (!obstacle) {
-      move_bot(FWD, 0.7);
-      moving = true;
-    }
+  if (check_timer(timer_1s)) {
+    set_timer(timer_1s, 1000);
+    // execute every second
   }
-  else {  // bot is moving
-    if (obstacle) {
+
+  // stop rotation at specified time
+  if (check_timer(timer_rotate)) {
+    if (state == ROTATING) {
       stop_bot();
-      rotate_bot_angle(-180);
-      moving = false;
+      state = STOPPED;
     }
   }
 
-  delay(50);
+  switch (state) {
+    case STOPPED:
+      if (!obstacle) {
+        move_bot(FWD, 0.7);
+        state = MOVING;
+      }
+      break;
+
+    case MOVING:
+      if (obstacle) {
+        stop_bot();
+        rotate_bot_angle(-180);
+        state = ROTATING;
+      }
+      break;
+
+    case ROTATING:
+      break;
+  }
+
+  distance = 0;  // clear distance in case the obstacle is no longer there
+
+  delay(30);
 }
